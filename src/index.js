4a7960a8cf0ef3b5990b00b15277f345c548460e
gouravkdeshmukh <gouravdeshmukhbzu@gmail.com>
2023-05-04 12:43:05 +0530

    setup routers and register middleware and controllers and setup logging mechanism using winston package

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const apiRoutes = require('./routes');
 
 const {
-    PORT
+    ServerConfig,
+    Logger
 } = require('./config/index.js');
+const logger = require('./config/logger-config');
+
 
 const app = express();
 
 app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+app.listen(ServerConfig.PORT, () => {
+    console.log(`listening on port ${ServerConfig.PORT}`);
+    logger.info("Successfully started server", {});
 });
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 
+const { InfoController } = require('../../controllers');
+
+
 const router = express.Router();
 
-router.get('/info', (req, res) => {
-    return res.json({
-        message: "ok"
-    })
-})
+router.get('/info', InfoController.info);
 
 
 module.exports = router;
